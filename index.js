dff65e97a9bc5363396a46b69c7894cf5aac319e
henry ventura <henry.ventura14@gmail.com>
2018-02-24 09:47:13 -0400

    socketIO

---
+++ index.js
@@ -0,0 +1,28 @@
+const path = require('path');
+const express = require('express');
+const app = express();
+
+//static files
+app.use(express.static(path.join(__dirname, 'public')));
+
+//settings
+app.set('port', process.env.PORT || 3000);
+
+//start server
+const server = app.listen(app.get('port'), () =>{
+  console.log('server on port', app.get('port'));
+});
+
+
+//websockets
+
+const SocketIO = require('socket.io');
+const io = SocketIO(server);
+
+io.on('connection', (socket) => {
+  console.log('new connection', socket.id);
+
+  socket.on('chat:message', (data) =>{
+    io.sockets.emit('chat:message', data);
+  })
+});
