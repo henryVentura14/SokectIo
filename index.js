b4db9d49d58ab5075ff4bb6a997e6dd6fe32d118
henryVentura14 <henry.ventura14@gmail.com>
2021-01-14 00:56:46 -0500

    add changes style button send, add change package.json

--- index.js
+++ index.js
@@ -1,28 +1,26 @@
-const path = require('path');
-const express = require('express');
-const app = express();
+const path = require('path')
+const express = require('express')
+const app = express()
 
 //static files
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')))
 
 //settings
-app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.PORT || 3000)
 
 //start server
-const server = app.listen(app.get('port'), () =>{
-  console.log('server on port', app.get('port'));
-});
-
+const server = app.listen(app.get('port'), () => {
+  console.log('server on port', app.get('port'))
+})
 
 //websockets
+const SocketIO = require('socket.io')
+const io = SocketIO(server)
 
-const SocketIO = require('socket.io');
-const io = SocketIO(server);
+io.on('connection', socket => {
+  console.log('new connection', socket.id)
 
-io.on('connection', (socket) => {
-  console.log('new connection', socket.id);
-
-  socket.on('chat:message', (data) =>{
-    io.sockets.emit('chat:message', data);
+  socket.on('chat:message', data => {
+    io.sockets.emit('chat:message', data)
   })
-});
+})
